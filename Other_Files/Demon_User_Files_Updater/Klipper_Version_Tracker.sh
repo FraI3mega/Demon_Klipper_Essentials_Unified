e4d5c4aaf5a6805a220191320ecaadb9624b781b
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2026-05-24 17:31:22 +0200

    Add files via upload
    Version 1 of automated system - USE WITH CARE!

---
+++ Other_Files/Demon_User_Files_Updater/Klipper_Version_Tracker.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# 3DPrintDemon Klipper Essentials Unified Klipper Version Tracker Script v1.0.0
+# https://github.com/3DPrintDemon/Demon_Klipper_Essentials_Unified
+
+# Define
+U="$USER"
+HDIR="/home/$U"
+KLDIR="/home/$U/printer_data/logs/klippy.log"
+KLIP="klipper"
+KVER="13"
+KNUM="371"
+
+######## KLIPPER VERIONS TRACKER ########
+
+var0=$(echo | grep -w --max-count=1 $KLDIR -e "Tracked URL:")
+var1=$(echo | grep -w --max-count=1 $KLDIR -e 'Git version:')
+
+echo $var0
+echo $var1
+
+trim_var0="${var0##*/}"
+
+trim_var1="${var1#*.}"
+trim_var2="${trim_var1%.*}"
+
+trim_var3="${var1#*-}"
+trim_var4="${trim_var3%%-*}"
+
+
+#echo Source value only = $trim_var0
+#echo Source value only = $trim_var2
+#echo Source value only = $trim_var4
+
+
+if [ "$trim_var0" = "$KLIP" ]; then
+    echo Firmware Type: "$trim_var0"
+    echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=is_klipper VALUE=True" >~/printer_data/comms/klippy.serial
+    echo Requesting Klipper set vairable is klipper to True
+
+    if [ "$trim_var2" -eq "$KVER" ]; then
+         echo Klipper version: Current
+    
+         if [ "$trim_var4" -lt "$KNUM" ]; then
+             echo Legacy Mode: ON
+             echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
+             echo Requesting Klipper set vairable legacy Klipper to True
+        
+         elif [ "$trim_var4" -eq "$KNUM" ] || [ "$trim_var4" -gt "$KNUM" ]; then
+             echo Legacy Mode: OFF
+             echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=False" >~/printer_data/comms/klippy.serial
+             echo Requesting Klipper set vairable legacy Klipper to False
+         fi
+
+    elif [ "$trim_var2" -lt "$KVER" ]; then
+        echo Legacy version of Klipper detected!
+        echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
+        echo Requesting Klipper set vairable legacy Klipper to True
+
+   
+    else
+    echo Klipper version is higher - someone needs to update this script!
+    fi
+
+else
+    echo Firmware type: "$trim_var0"
+    echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=is_klipper VALUE=False" >~/printer_data/comms/klippy.serial
+    echo Requesting Klipper set vairable is klipper to False
+fi
+
+
+
+
+
+
+
+
+
