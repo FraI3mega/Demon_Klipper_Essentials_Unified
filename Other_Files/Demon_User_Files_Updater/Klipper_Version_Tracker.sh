36cc1f9ad10889e377317a3ff083c03428fc0da4
3DPrintDemon <122202359+3DPrintDemon@users.noreply.github.com>
2026-06-01 23:28:34 +0200

    Update Klipper Version Tracker script to v1.0.4

--- Other_Files/Demon_User_Files_Updater/Klipper_Version_Tracker.sh
+++ Other_Files/Demon_User_Files_Updater/Klipper_Version_Tracker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 3DPrintDemon Klipper Essentials Unified Klipper Version Tracker Script v1.0.3
+# 3DPrintDemon Klipper Essentials Unified Klipper Version Tracker Script v1.0.4
 # https://github.com/3DPrintDemon/Demon_Klipper_Essentials_Unified
 
 # Define
@@ -8,7 +8,7 @@ U="$USER"
 HDIR="/home/$U"
 KLDIR="/home/$U/printer_data/logs/klippy.log"
 KLIP="klipper"
-SKlIP="klipper.git"
+LKVER="12"
 KVER="13"
 KNUM="623"
 
@@ -21,6 +21,7 @@ echo $var0
 echo $var1
 
 trim_var0="${var0##*/}"
+trim_var00="${trim_var0%.*}"
 
 trim_var1="${var1#*.}"
 trim_var2="${trim_var1%.*}"
@@ -29,43 +30,48 @@ trim_var3="${var1#*-}"
 trim_var4="${trim_var3%%-*}"
 
 
-#echo Source value only = $trim_var0
-#echo Source value only = $trim_var2
-#echo Source value only = $trim_var4
+# echo Source value only = $trim_var0
+# echo Source value only = $trim_var2
+# echo Source value only = $trim_var4
 
+if [ "$trim_var0" = "$KLIP" ] || [ "$trim_var0" = "$KLIP.git" ]; then
+    echo Firmware Type: "$trim_var0"
 
-if [ "$trim_var0" = "$KLIP" ] || [ "$trim_var0" = "$SKLIP" ]; then
-    if [ "$trim_var0" = "$SKLIP" ]; then
-        echo Firmware Type: "${trim_var0%.*}"
-    else
-        echo Firmware Type: "$trim_var0"
-    fi
-    
+    if [ "$trim_var0" = "$KLIP.git" ]; then
+        echo Non-standard Tracked Klipper URL detected
+    fi  
+
     echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=is_klipper VALUE=True" >~/printer_data/comms/klippy.serial
-    echo Requesting Klipper set variable is klipper to True
+    echo Requesting Klipper set variable is klipper to True  
 
-    if [ "$trim_var2" -eq "$KVER" ]; then
-         echo Klipper version: Current
-    
-         if [ "$trim_var4" -lt "$KNUM" ]; then
-             echo Legacy Mode: ON
-             echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
-             echo Requesting Klipper set variable legacy Klipper to True
-        
-         elif [ "$trim_var4" -eq "$KNUM" ] || [ "$trim_var4" -gt "$KNUM" ]; then
-             echo Legacy Mode: OFF
-             echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=False" >~/printer_data/comms/klippy.serial
-             echo Requesting Klipper set variable legacy Klipper to False
-         fi
-
-    elif [ "$trim_var2" -lt "$KVER" ]; then
-        echo Legacy version of Klipper detected!
+    if [ "$trim_var2" != "$KVER" ] && [ "$trim_var2" != "$LKVER" ] ; then
+        echo Non-standard versioning detected - Legacy Mode: ON
         echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
         echo Requesting Klipper set variable legacy Klipper to True
-
-   
+        
     else
-    echo Klipper version is higher - someone needs to update this script!
+        if [ "$trim_var2" -eq "$KVER" ]; then
+        echo Klipper version: Current
+    
+            if [ "$trim_var4" -lt "$KNUM" ]; then
+                echo Legacy Mode: ON
+                echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
+                echo Requesting Klipper set variable legacy Klipper to True
+        
+            elif [ "$trim_var4" -eq "$KNUM" ] || [ "$trim_var4" -gt "$KNUM" ]; then
+               echo Legacy Mode: OFF
+               echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=False" >~/printer_data/comms/klippy.serial
+               echo Requesting Klipper set variable legacy Klipper to False
+            fi
+
+        elif [ "$trim_var2" -lt "$KVER" ]; then
+            echo Legacy version of Klipper detected!
+            echo "SET_GCODE_VARIABLE MACRO=_CORE_VARS VARIABLE=legacy_klipper VALUE=True" >~/printer_data/comms/klippy.serial
+            echo Requesting Klipper set variable legacy Klipper to True
+
+        else
+        echo Klipper version is higher - someone needs to update this script!
+        fi
     fi
 
 else
